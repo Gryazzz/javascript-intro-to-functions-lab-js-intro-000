dc120bc5831ff2e2de4e21cc767fb5bc77bcf8d2
Sonik S <gryazzz@gmail.com>
2017-12-05 02:42:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ function whisper(string) {
   return string.toLowerCase()
 }
 
+function logShout(string) {
+  return console.log(string.toUpperCase());
+    
+}
+
+
 function sayHiToGrandma(string) {
   if(string.toLowerCase() === string) {
     return "I can't hear you!"
